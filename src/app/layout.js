5ee2618ac1d303f830296717c141bfab0fb0273d
hariprasad <devwitharii@gmail.com>
2025-02-14 21:51:51 +0530

    framer motion added

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* <header>Logo</header> */}
         {children}
+        {/* <footer>footer</footer> */}
       </body>
     </html>
   );
